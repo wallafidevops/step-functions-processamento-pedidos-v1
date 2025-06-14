7da67551fd9b87453946d6512c46f7e88deee961
wallafi01 <wallafisantos98@outlook.com>
2025-06-14 16:45:03 -0300

    up

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "rm-state-6189"
     key            = "step-functions/processamento-pedido/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
   }
 }
